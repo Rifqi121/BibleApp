eb25adc68bea4c9e1c24726961dc8999fdd34e9b
Rifqi121 <rfauzanadzikra2.siswa@smkn4bdg.sch.id>
2022-03-17 21:43:38 +0700

    add setting

--- app/src/main/java/com/example/bible/ui/setting/SettingFragment.java
+++ app/src/main/java/com/example/bible/ui/setting/SettingFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.bible.R;
 import com.example.bible.databinding.FragmentSettingBinding;
