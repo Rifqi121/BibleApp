fa2f5808a3f89e30d43de4049b085a77416a0d3a
Rifqi121 <rfauzanadzikra2.siswa@smkn4bdg.sch.id>
2022-03-17 16:48:20 +0700

    first commit

---
+++ app/src/main/java/com/example/bible/Book.java
@@ -0,0 +1,13 @@
+package com.example.bible;
+
+public class Book {
+    String title;
+
+    public Book(String title){
+        this.title = title;
+    }
+
+    public java.lang.String getTitle() {
+        return title;
+    }
+}
---
+++ app/src/main/java/com/example/bible/ui/book/BookFragment.java
@@ -0,0 +1,45 @@
+package com.example.bible.ui.book;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.bible.R;
+import com.example.bible.databinding.FragmentBookBinding;
+
+public class BookFragment extends Fragment {
+
+    private BookViewModel bookViewModel;
+    private FragmentBookBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        bookViewModel =
+                new ViewModelProvider(this).get(BookViewModel.class);
+
+        binding = FragmentBookBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        bookViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
