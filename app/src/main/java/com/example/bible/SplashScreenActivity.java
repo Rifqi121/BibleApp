4a9f34cb5462a8b73eb03d921e4a518f87da4d3e
Ramdan Rohendi <ramdanrohendi@gmail.com>
2022-03-22 16:37:05 +0700

    Add setting Languange

--- app/src/main/java/com/example/bible/SplashScreenActivity.java
+++ app/src/main/java/com/example/bible/SplashScreenActivity.java
@@ -3,8 +3,13 @@ package com.example.bible;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.DisplayMetrics;
+
+import java.util.Locale;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -14,11 +19,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                String codeLocal = SettingPreferences.getCodeLanguage(getBaseContext());
+                Configuration conf = getResources().getConfiguration();
+                conf.locale = new Locale(codeLocal);
+                DisplayMetrics metrics = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(metrics);
+                Resources resources = new Resources(getAssets(), metrics, conf);
+
                 Intent landing = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(landing);
                 finish();
--- app/src/main/java/com/example/bible/ui/setting/SettingFragment.java
+++ app/src/main/java/com/example/bible/ui/setting/SettingFragment.java
@@ -1,35 +1,50 @@
 package com.example.bible.ui.setting;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bible.R;
+import com.example.bible.SettingPreferences;
 import com.example.bible.databinding.FragmentSettingBinding;
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
+import java.util.Locale;
 
 public class SettingFragment extends Fragment {
 
     private SettingViewModel settingViewModel;
     private FragmentSettingBinding binding;
-    List<String> listDataHeader;
-    MaterialCardView cardView;
-    ImageButton show;
-    LinearLayout hiddenLayout;
+    private MaterialCardView cardView;
+    private ImageButton show;
+    private LinearLayout hiddenLayout;
+    private RadioGroup rgLanguage;
+    private RadioButton rbEnglish;
+    private RadioButton rbRussian;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingViewModel =
@@ -41,7 +56,18 @@ public class SettingFragment extends Fragment {
         cardView = root.findViewById(R.id.cardView);
         show = root.findViewById(R.id.button_show);
         hiddenLayout = root.findViewById(R.id.linear_layout);
+        rgLanguage = root.findViewById(R.id.rgLanguage);
+        rbEnglish = root.findViewById(R.id.radio_eng);
+        rbRussian = root.findViewById(R.id.radio_rus);
 
+        setDataSetting();
+
+        rgLanguage.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                setLanguage();
+            }
+        });
 
         show.setOnClickListener(v -> {
             if (hiddenLayout.getVisibility() == root.VISIBLE) {
@@ -65,6 +91,30 @@ public class SettingFragment extends Fragment {
         return root;
     }
 
+    private void setLanguage() {
+        int selectedId = rgLanguage.getCheckedRadioButtonId();
+        String codeLocal = "en";
+
+        if (selectedId == rbRussian.getId()) {
+            codeLocal = "ba";
+        }
+
+        SettingPreferences.setCodeLanguage(getActivity().getBaseContext(), codeLocal);
+        Snackbar.make(getActivity().findViewById(android.R.id.content), getResources().getString(R.string.please_restart), Snackbar.LENGTH_LONG).show();
+    }
+
+    private void setDataSetting() {
+        String codeLang = SettingPreferences.getCodeLanguage(getActivity().getBaseContext());
+
+        // Language
+        if (codeLang.equals("en")) {
+            rbEnglish.setChecked(true);
+        }
+
+        if (codeLang.equals("ba")) {
+            rbRussian.setChecked(true);
+        }
+    }
 
     @Override
     public void onDestroyView() {
