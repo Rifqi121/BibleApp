0e2223a7458d6384491e58e4b9c5c7bfef1d7d80
Rifqi121 <rfauzanadzikra2.siswa@smkn4bdg.sch.id>
2022-03-22 22:48:03 +0700

    adding progressBar

--- app/src/main/java/com/example/bible/PodcastActivity.java
+++ app/src/main/java/com/example/bible/PodcastActivity.java
@@ -2,15 +2,19 @@ package com.example.bible;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class PodcastActivity extends AppCompatActivity {
 
     public WebView mWebView;
+    private ProgressBar spinner;
+    String VisibilityWebViewInitialUse = "show";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +28,30 @@ public class PodcastActivity extends AppCompatActivity {
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        // Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
+        spinner = findViewById(R.id.progressBar1);
+        mWebView.setWebViewClient(new PodcastActivity.CustomWebViewClient());
 
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView mWebView, String url, Bitmap favicon) {
+            // only make it invisible the FIRST time the app is run
+            if (VisibilityWebViewInitialUse.equals("show")) {
+                mWebView.setVisibility(mWebView.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            VisibilityWebViewInitialUse = "hide";
+            spinner.setVisibility(View.GONE);
+
+            view.setVisibility(mWebView.VISIBLE);
+            super.onPageFinished(view, url);
+
+        }
     }
 }
--- app/src/main/java/com/example/bible/SermonsActivity.java
+++ app/src/main/java/com/example/bible/SermonsActivity.java
@@ -2,19 +2,24 @@ package com.example.bible;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class SermonsActivity extends AppCompatActivity {
 
     public WebView mWebView;
+    private ProgressBar spinner;
+    String VisibilityWebViewInitialUse = "show";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_podcast);
+        setContentView(R.layout.activity_sermons);
 
         mWebView = (WebView) findViewById(R.id.webview);
         mWebView.loadUrl("https://www.youtube.com/channel/UCQ_1ClPcArkHtl3DHrvbqXg");
@@ -23,8 +28,39 @@ public class SermonsActivity extends AppCompatActivity {
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        // Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
+        spinner = findViewById(R.id.progressBar1);
+        mWebView.setWebViewClient(new SermonsActivity.CustomWebViewClient());
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView mWebView, String url, Bitmap favicon) {
+            // only make it invisible the FIRST time the app is run
+            if (VisibilityWebViewInitialUse.equals("show")) {
+                mWebView.setVisibility(mWebView.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            VisibilityWebViewInitialUse = "hide";
+            spinner.setVisibility(View.GONE);
+
+            view.setVisibility(mWebView.VISIBLE);
+            super.onPageFinished(view, url);
+
+        }
     }
 }
